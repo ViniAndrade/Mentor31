e1cee263d3f343bd7e3aa5c6c89ddc8097eb5e00
Vinicius <vini.boy011@gmail.com>
2017-05-31 14:31:44 -0300

    editar perfil e icones atualizados

---
+++ app/Http/Controllers/PerfilController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mentor\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Mentor\Http\Requests;
+
+class PerfilController extends Controller
+{
+    
+	  public function index()
+    {
+        return 'oi';
+}
